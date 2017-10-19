4e575e3e2958fb1c64477deb5e8ed837c8edddd4
Guilherme Sardá Pinheiro <guisardapinheiro@gmail.com>
2017-10-19 20:19:35 -0200

    teste aplicação properties

--- src/main/java/com/my/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/my/api/MeuPrimeiroProjetoApplication.java
@@ -1,13 +1,26 @@
 package com.my.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
+	
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
 		System.out.println("teste");
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("### Quantidade de elementos por página = " + this.qtdPorPagina);			
+		};
 	}
 }
